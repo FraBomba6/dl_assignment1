fix: add argument to softmax

notebooks/libs/net_utils.py
@@ -65,6 +65,6 @@ def build_output_components(in_channels):
     ).to(DEVICE)
     classes = nn.Sequential(
         nn.Conv2d(in_channels, 12, 1),
-        nn.Softmax()
+        nn.Softmax(dim=1)
     ).to(DEVICE)
     return confidence, box, classes