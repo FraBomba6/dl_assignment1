edf0a5c77bff05be6526d09f03c209a1783fbc60
FraBomba6 <bombasseidebona.francesco@spes.uniud.it>
2022-12-05 10:55:20 +0100

    feat: mps device for Mac ARM

--- notebooks/libs/utils.py
+++ notebooks/libs/utils.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Initializing torch device according to hardware available
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
 IMG_SIZE = 256
 
