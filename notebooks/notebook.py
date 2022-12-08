11b7eb9b2839bbcc558ad453436d0186d16e2661
bomb697 <50354770+bomb697@users.noreply.github.com>
2022-12-08 23:38:06 +0100

    fix: print arguments

--- notebooks/notebook.py
+++ notebooks/notebook.py
@@ -128,7 +128,7 @@ def train(num_epochs, print_interval=10):
 
         console.log("\nTesting accuracy\n")
         accuracy = test_accuracy()
-        console.log('For epoch', epoch + 1, 'the test accuracy over the whole test set is %d' % accuracy)
+        console.log('For epoch', epoch + 1, 'the test accuracy over the whole test set is %d %%' % accuracy[0])
 
         # we want to save the model if the accuracy is the best
         if accuracy > best_accuracy:
