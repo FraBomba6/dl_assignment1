f404fff54c004177d41e4f0437b700de5a59d307
FraBomba6 <bombasseidebona.francesco@spes.uniud.it>
2022-12-08 15:38:46 +0100

    feat: Non Max Suppression and mAP

--- notebooks/libs/utils.py
+++ notebooks/libs/utils.py
@@ -1,5 +1,7 @@
 import math
 import os
+from collections import Counter
+
 import torch
 import torchvision
 from torchvision import transforms
@@ -14,6 +16,9 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(_
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
 IMG_SIZE = 256
+
+CONFIDECNE_TRESHOLD = 0.5
+IOU_TRESHOLD = 0.5
 
 
 def collate_fn(batch):
@@ -83,7 +88,7 @@ def to_center_coords(boxes):
     return new_boxes
 
 
-def i_over_u(batched_predicted_boxes, batched_target_boxes):
+def i_over_u(batched_predicted_boxes: torch.Tensor, batched_target_boxes: torch.Tensor):
     """
     Compute intersection over union of batched Tensors
     """
@@ -106,3 +111,80 @@ def i_over_u(batched_predicted_boxes, batched_target_boxes):
     union_area = pred_area + target_area - intersection_area
 
     return intersection_area/(union_area + 1e-8)
+
+
+def non_max_suppression(bounding_boxes: list):
+    """
+    Perform Non Max Suppression to find which predicted bounding box is the best
+    :param bounding_boxes: list of lists of the type [class, confidence, x, y, h, w]
+    :return:
+    """
+    # Purge boxes which confidence is too low
+    bounding_boxes = [box for box in bounding_boxes if box[1] < CONFIDECNE_TRESHOLD]
+    bounding_boxes = sorted(bounding_boxes, key=lambda x: x[1], reverse=True)
+    boxes = []
+    while bounding_boxes:
+        max_confidence_box = bounding_boxes.pop(0)
+        boxes.append(max_confidence_box)
+
+        for index, box in enumerate(bounding_boxes):
+            iou = i_over_u(torch.tensor(max_confidence_box[2:], device=DEVICE), torch.tensor(box[2:], device=DEVICE))
+            if box[0] == max_confidence_box[0] and iou >= IOU_TRESHOLD:
+                bounding_boxes.pop(index)
+    return boxes
+
+
+def mean_average_precision(predictions, targets):
+    """
+    Computes mAP to evaluate model
+    :param predictions: list of lists of the type [image_index, class, confidence, x, y, w, h]
+    :param targets: as the previous but with only ture boxes
+    :return: mAP score
+    """
+    average_precisions = []
+
+    for cla in range(13):
+        ground_truths = [ground_truth for ground_truth in targets if ground_truth[1] == cla]
+        total_true_bounding_boxes = len(ground_truths)
+
+        if total_true_bounding_boxes == 0:
+            continue
+
+        detections = [detection for detection in predictions if detection[1] == cla]
+        detections.sort(key=lambda x: x[2], reverse=True)
+
+        TP = torch.zeros(len(detections), device=DEVICE)
+        FP = torch.zeros(len(detections), device=DEVICE)
+
+        amount_of_bounding_boxes = Counter([ground_truth[0] for ground_truth in ground_truths])
+        for key, val in amount_of_bounding_boxes.items():
+            amount_of_bounding_boxes[key] = torch.zeros(val, device=DEVICE)
+
+        for detection_id, detection in enumerate(detections):
+            filtered_ground_truths = [box for box in ground_truths if box[0] == detection[0]]
+            best_iou = 0
+
+            for ground_truth_id, ground_truth in enumerate(filtered_ground_truths):
+                iou = i_over_u(
+                    torch.tensor(detection[3:], device=DEVICE),
+                    torch.tensor(ground_truth[3:], device=DEVICE)
+                )
+
+                if iou > best_iou:
+                    best_iou = iou
+                    best_ground_truth_id = ground_truth_id
+
+            if best_iou > IOU_TRESHOLD and amount_of_bounding_boxes[detection[0]][best_ground_truth_id] == 0:
+                    TP[detection_id] = 1
+                    amount_of_bounding_boxes[detection[0]][best_ground_truth_id] = 1
+            else:
+                FP[detection_id] = 1
+
+        TP_cumsum = torch.cumsum(TP, dim=0)
+        FP_cumsum = torch.cumsum(FP, dim=0)
+        recalls = TP_cumsum / (total_true_bounding_boxes + 1e-6)
+        recalls = torch.cat((torch.tensor([0], device=DEVICE), recalls))
+        precisions = torch.divide(TP_cumsum, (TP_cumsum + FP_cumsum + 1e-6))
+        precisions = torch.cat((torch.tensor([1], device=DEVICE), precisions))
+        average_precisions.append(torch.trapz(precisions, recalls))
+        return sum(average_precisions) / len(average_precisions), recalls, precisions
