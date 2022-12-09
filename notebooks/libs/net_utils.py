c78b45b038fe935b5105fa73521f39bacfb4791c
bomb697 <50354770+bomb697@users.noreply.github.com>
2022-12-09 01:08:36 +0100

    wip: adjust output layer

--- notebooks/libs/net_utils.py
+++ notebooks/libs/net_utils.py
@@ -74,7 +74,7 @@ def build_output_components(in_channels, b=2):
     ).to(DEVICE)
     box = nn.Sequential(
         nn.Conv2d(in_channels, total_boxes_layers, 1),
-        nn.Conv2d(total_boxes_layers, total_boxes_layers, 9, padding='same'),
+        nn.Conv2d(total_boxes_layers, total_boxes_layers, 3, padding='same'),
         nn.Conv2d(total_boxes_layers, total_boxes_layers, 1)
     ).to(DEVICE)
     classes = nn.Sequential(
