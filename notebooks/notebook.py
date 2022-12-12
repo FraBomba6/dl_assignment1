45b6aedc77f7ee17f2e6a6907364c4c6e03c7e7f
bomb697 <50354770+bomb697@users.noreply.github.com>
2022-12-12 21:53:45 +0100

    wip: back to previous version

--- notebooks/notebook.py
+++ notebooks/notebook.py
@@ -65,36 +65,29 @@ class ObjectDetectionModel(nn.Module):
         self.convolutions.append(net_utils.build_simple_convolutional_block(256, 256, conv_kernel=1))
         self.convolutions.append(net_utils.build_simple_convolutional_block(256, 512, pool_kernel=2))
         # 31 x 31
-        for i in range(1):
+        for i in range(5):
             self.convolutions.append(net_utils.build_simple_convolutional_block(512, 256, conv_kernel=1))
             self.convolutions.append(net_utils.build_simple_convolutional_block(256, 512))
         self.convolutions.append(net_utils.build_simple_convolutional_block(512, 512, conv_kernel=1))
         self.convolutions.append(net_utils.build_simple_convolutional_block(512, 1024, pool_kernel=2))
         # 15 x 15
-        for i in range(1):
+        for i in range(5):
             self.convolutions.append(net_utils.build_simple_convolutional_block(1024, 512, conv_kernel=1))
             self.convolutions.append(net_utils.build_simple_convolutional_block(512, 1024))
-        self.convolutions.append(net_utils.build_simple_convolutional_block(1024, 512, pool_kernel=2))
+        self.convolutions.append(net_utils.build_simple_convolutional_block(1024, 1024, pool_kernel=2))
+        for i in range(3):
+            self.convolutions.append(net_utils.build_simple_convolutional_block(1024, 1024))
         # 7 x 7
-        # self.output = net_utils.build_output_components(1024)
-        self.output = nn.Sequential(
-            nn.Linear(512*7*7, 64*7*7),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(64*7*7, 23*7*7)
-        )
+        self.output = net_utils.build_output_components(1024)
 
     def forward(self, x):
         x = self.convolutions(x)
-        # x = [
-        #     self.output[0](x),
-        #     self.output[1](x),
-        #     self.output[2](x)
-        # ]
-        # return torch.cat(x, 1)
-        x = x.view(-1, 512*7*7)
-        x = self.output(x)
-        return x.reshape(-1, 7, 7, 23)
+        x = [
+            self.output[0](x),
+            self.output[1](x),
+            self.output[2](x)
+        ]
+        return torch.cat(x, 1)
 
 
 # %%
