15c9e97d413740a326aa3d1c7f45c984bb8674df
bomb697 <50354770+bomb697@users.noreply.github.com>
2022-12-12 20:16:08 +0100

    wip: output layer

--- notebooks/notebook.py
+++ notebooks/notebook.py
@@ -79,10 +79,10 @@ class ObjectDetectionModel(nn.Module):
             self.convolutions.append(net_utils.build_simple_convolutional_block(1024, 1024))
         # 7 x 7
         # self.output = net_utils.build_output_components(1024)
-        self.fc1 = nn.Linear(1024*7*7, 512*7*7)
+        self.fc1 = nn.Linear(1024*7*7, 256*7*7)
         self.relu = nn.ReLU()
         self.drop = nn.Dropout()
-        self.fc2 = nn.Linear(512*7*7, 23*7*7)
+        self.fc2 = nn.Linear(256*7*7, 23*7*7)
         # self.output = nn.Sequential(
         #     nn.Linear(1024*7*7, 512*7*7),
         #     nn.ReLU(),
